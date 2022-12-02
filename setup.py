9461f2c929a68477968f95a564c8cdcf087e04b4
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-12-02 21:11:23 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A simple python vector package.'
+LONG_DESCRIPTION = 'A package that allows vector operators specifically aimed at machine learning.'
+
+# Setting up
+setup(
+    name="vector-ai-ml",
+    version=VERSION,
+    author="AtlasAerospace (Alexander Armitage)",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'math', 'ai', 'ml', 'artificialintelligence'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
